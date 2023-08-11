0209158410fb6356fda7ca8e79f80bbc46220263
carlosfruitcup <40542088+carlosfruitcup@users.noreply.github.com>
2023-08-11 14:15:14 -0400

    Updated bone_gizmo.gd
    fixed preload paths

--- addons/BoneGizmo-patch-1/bone_gizmo.gd
+++ addons/BoneGizmo-patch-1/bone_gizmo.gd
@@ -2,7 +2,8 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("BoneGizmo", "Spatial", preload("bone_gizmo_node.gd"), preload("bone_gizmo.png"))
+	add_custom_type("BoneGizmo", "Spatial", preload("res://addons/BoneGizmo-patch-1/bone_gizmo_node.gd"), 
+	preload("res://addons/BoneGizmo-patch-1/bone_gizmo.png"))
 
 func _exit_tree():
 	remove_custom_type("BoneGizmo")
