7f8ff25f40f73ddf3becc15965acaa62a33aec8f
carlosfruitcup <40542088+carlosfruitcup@users.noreply.github.com>
2023-08-11 14:07:16 -0400

    Separate Folder
    Moves the addon to its own folder for organization and so godot can recognize the addon

--- addons/BoneGizmo-patch-1/plugin.gd
+++ addons/BoneGizmo-patch-1/plugin.gd
@@ -5,7 +5,7 @@ var dock
 
 func _enter_tree():
 	add_custom_type("BoneGizmo", "Spatial", preload("bone_gizmo_node.gd"), preload("icon.png"))
-	dock = preload("res://addons/bone_gizmo/dock.tscn").instance()
+	dock = preload("res://addons/BoneGizmo-patch-1/dock.tscn").instance()
 	add_control_to_dock( DOCK_SLOT_LEFT_UL, dock)
 
 func _exit_tree():
